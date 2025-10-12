2b7d3676c25c3ee579d9430ded410a96a7754339
Danilo Santos <daniloddocarmo16@icloud.com>
2025-10-12 17:52:53 -0300

    test

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CadastroEstudantes.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
