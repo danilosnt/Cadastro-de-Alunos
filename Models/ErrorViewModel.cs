017e7089887891d7f28c8b583832b788cfcc14a9
Danilo Santos <daniloddocarmo16@icloud.com>
2025-10-12 17:41:24 -0300

    Add files via upload

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CadastroEstudantes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
