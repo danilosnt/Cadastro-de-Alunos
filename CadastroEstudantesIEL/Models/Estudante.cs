869cfecc9d173c1cca6ada95213b07131517f43b
Danilo Santos <daniloddocarmo16@icloud.com>
2025-10-12 19:46:50 -0300

    Adiciona .gitignore e commita arquivos do projeto

---
+++ CadastroEstudantesIEL/Models/Estudante.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CadastroEstudantesIEL.Models
+{
+    public class Estudante
+    {
+        [Key]
+        public int Id { get; set}
+
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        [StringLength(100, ErrorMessage = "O nome não pode exceder 100 caracteres")]
+        [Display(Name = "Nome Completo")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo CPF é obrigatório")]
+        [Display(Name = "CPF")]
+        public string CPF { get; set; }
+
+        [Required(ErrorMessage = "O campo Endereço é obrigatório")]
+        [StringLength(200, ErrorMessage = "O endereço não pode exceder 200 caracteres")]
+        [Display(Name = "Endereço")]
+        public string Endereco { get; set; }
+
+        [Required(ErrorMessage = "O campo Data de Conclusão é obrigatório")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Data de Conclusão")]
+        public DateTime DataConclusao { get; set; }
+    }
+}
